a110d8b0675e3a145c804e2ca9aa31046d4c79db
tomiaJO <t.koncz@gmail.com>
2018-02-04 16:03:35 +0100

    -

---
+++ scrape_nba_schedules.R
@@ -0,0 +1,55 @@
+#library(rvest)
+library(data.table)
+library(XML)
+library(xml2)
+
+base_url <- "https://www.basketball-reference.com/leagues/NBA_xYYYYx_games-xmonthx.html"
+
+complete_url <- function(year, month) {
+  
+  url <- sub(pattern = "xYYYYx", replacement = year, x = base_url)
+  url <- sub(pattern = "xmonthx", replacement = month, x = url)
+ 
+  return(url)
+}
+
+months <- c("october", "november", "december", "january", "february",
+            "march", "april", "may", "june")
+
+for(m in months) {
+  html <- xml2::read_html(complete_url("2017", m))
+  node <- rvest::html_node(html, "table")
+  table <- rvest::html_table(node, header = TRUE)  
+}
+
+
+
+url <- paste(base_url, as.character(0), sep = "")
+df_injuries = readHTMLTable(url, header = T, which = 1, stringsAsFactors = F)
+
+for(i in seq(from= 25,to= 22875,by= 25)) {
+  
+  url <- paste(base_url, as.character(i), sep = "")
+  injuries = readHTMLTable(url, header = T, which = 1, stringsAsFactors = F)
+  
+  df_injuries <- rbind(df_injuries, injuries)
+  rm(injuries)
+  print(i) #tracking progress
+}
+
+# removing variables not used anymore
+rm(base_url)
+rm(url)
+rm(i)
+
+injury.table <- data.table(df_injuries)
+rm(df_injuries)
+
+head(injury.table)
+
+str_to_replace <- substring(injury.table[5, 4], 1, 2)
+
+injury.table[, Acquired := gsub(pattern = str_to_replace, replacement = "", x = Acquired)]
+injury.table[, Relinquished := gsub(pattern = str_to_replace, replacement = "", x = Relinquished)]
+
+fwrite(injury.table, file = "injuriesTable.csv")

a9df353b3340d5dcb00e9c3a0c1f3d2c4ad39a43
tomiaJO <t.koncz@gmail.com>
2018-10-15 22:57:45 +0200

    :basketball: Plot Russ' in game stats vs. game's final point diff.
    Includes formatting fix for plotted columns

--- Westbrook Triple-Doubles/Functions/westbrook_triple_double_functions.R
+++ Westbrook Triple-Doubles/Functions/westbrook_triple_double_functions.R
@@ -33,9 +33,77 @@ fixRawRows <- function(dt) {
   dt[G != "Rk"]
 }
 
+
 addResultColumns_ <- function(dt) {
   dt %>%
     .[, `Won?` := grepl("^W", Result)] %>% 
-    .[, `Score Difference` := as.numeric(str_match(Result, "\\((\\+|-)(.+)\\)")[, 3])] %>%
-    .[, `Score Difference` := ifelse(!`Won?`, -1, 1) * `Score Difference`]
+    .[, `Point Difference` := as.numeric(str_match(Result, "\\((\\+|-)(.+)\\)")[, 3])] %>%
+    .[, `Point Difference` := ifelse(!`Won?`, -1, 1) * `Point Difference`]
+}
+
+
+fixColumnFormats_ <- function(dt) {
+  dt %>% 
+    .[, `:=`(G     = as.numeric(G),
+             Date  = as.Date(Date, format = "%Y-%m-%d"),
+             FG    = ifelse(FG    == "Did Not Play", NA, as.numeric(FG)), 
+             FGA   = ifelse(FGA   == "Did Not Play", NA, as.numeric(FGA)), 
+             `FG%` = ifelse(`FG%` == "Did Not Play", NA, as.numeric(`FG%`)), 
+             `3P`  = ifelse(`3P`  == "Did Not Play", NA, as.numeric(`3P`)), 
+             `3PA` = ifelse(`3PA` == "Did Not Play", NA, as.numeric(`3PA`)), 
+             `3P%` = ifelse(`3P%` == "Did Not Play", NA, as.numeric(`3P%`)), 
+             FT    = ifelse(FT    == "Did Not Play", NA, as.numeric(FT)), 
+             FTA   = ifelse(FTA   == "Did Not Play", NA, as.numeric(FTA)), 
+             `FT%` = ifelse(`FT%` == "Did Not Play", NA, as.numeric(`FT%`)), 
+             ORB   = ifelse(ORB   == "Did Not Play", NA, as.numeric(ORB)),  
+             DRB   = ifelse(DRB   == "Did Not Play", NA, as.numeric(DRB)), 
+             TRB   = ifelse(TRB   == "Did Not Play", NA, as.numeric(TRB)), 
+             AST   = ifelse(AST   == "Did Not Play", NA, as.numeric(AST)), 
+             STL   = ifelse(STL   == "Did Not Play", NA, as.numeric(STL)), 
+             BLK   = ifelse(BLK   == "Did Not Play", NA, as.numeric(BLK)), 
+             TOV   = ifelse(TOV   == "Did Not Play", NA, as.numeric(TOV)), 
+             PF    = ifelse(PF    == "Did Not Play", NA, as.numeric(PF)), 
+             PTS   = ifelse(PTS   == "Did Not Play", NA, as.numeric(PTS)), 
+             GmSc  = ifelse(GmSc  == "Did Not Play", NA, as.numeric(GmSc)))]
+}
+
+
+plotInGameStatsVsPointDiff <- function(dt) {
+
+  in_game_statistics <- getListOfInGameStatistics()
+  
+  dt %>% 
+    .[, mget(c("Point Difference", "Won?", in_game_statistics))] %>% 
+    melt(id.vars = c("Point Difference", "Won?"), measure.vars = in_game_statistics) %>% 
+    ggplot(aes(x = value, y = `Point Difference`)) +
+    geom_point(aes(color = `Won?`)) +
+    facet_wrap(~variable, scales = "free_x", ncol = 6) +
+    geom_smooth(method = "lm") +
+    labs(title = "Russ' in game stats vs. final point difference",
+         x     = "") +
+    scale_color_manual(values=c("gray82", "#0072CE")) +
+    theme_minimal() +
+    # theme(panel.grid.minor = element_blank(),
+    #       panel.grid.major = element_blank()) +
+    theme(text          = element_text(size = 11, family = "OCR A Extended")) +
+    theme(plot.title    = element_text(size = 18),
+          plot.caption  = element_text(color = "gray60")) +
+    theme(legend.position = "none")
+  
+  ggsave(
+    filename = "Westbrook Triple-Doubles/Figures/westbrook_in_game_stats_vs_point_difference.png",
+    device = "png", 
+    dpi = 600, 
+    width = 12, 
+    height = 8
+  )
+}
+
+
+getListOfInGameStatistics <- function() {
+  c(
+    "FG",  "3P",  "FT",  "TRB", "AST", "PTS",
+    "FGA", "3PA", "FTA", "ORB", "TOV", "PF",
+    "FG%", "3P%", "FT%", "DRB", "STL", "BLK"
+  )
 }
--- Westbrook Triple-Doubles/R/nba-westbrook-triple-doubles.R
+++ Westbrook Triple-Doubles/R/nba-westbrook-triple-doubles.R
@@ -2,9 +2,16 @@ source("Westbrook Triple-Doubles/Global.R")
 
 seasons <- c(2017:2018)
 
+## ----
 westbrook_raw_data_from_br <- map_df(seasons, getWestbrookRawStatsFromBR)
 
 fixRawColumns_(westbrook_raw_data_from_br)
 westbrook_data_from_br <- fixRawRows(westbrook_raw_data_from_br)
 
 addResultColumns_(westbrook_data_from_br)
+
+## ----
+fixColumnFormats_(westbrook_data_from_br)
+
+## ----
+plotInGameStatsVsPointDiff(westbrook_data_from_br)
